21e0190d407306054eae58cbdb5e960b140797f6
daniel castro <djcadaniel@gmail.com>
2024-05-13 17:13:11 -0500

    add selectedID

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,13 +2,14 @@ import type { MenuItem } from "../types"
 
 type MenuItemProps = {
   item: MenuItem
-  addItem: ()=>void
+  addItem: (item: MenuItem) => void
 }
 
-export default function MenuItem({item} : MenuItemProps) {
+export default function MenuItem({item, addItem} : MenuItemProps) {
   return (
     <button
       className="border-2 border-teal-400 hover:border-teal-200 w-full p-2 flex justify-between"
+      onClick={() => addItem(item)}
     >
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,12 +1,29 @@
 import { useState } from "react";
-import { OrderItem } from "../types";
+import { MenuItem, OrderItem } from "../types";
 
 export default function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([]);
 
-  const addItem = () => {
-    console.log("agregando...");
+  const addItem = (item: MenuItem) => {
+    // console.log(item);
+    const itemExist = order.find((itemOrder) => itemOrder.id === item.id);
+    console.log(itemExist);
+    //   if (itemExist) {
+    //     const updatedOrder = order.map((orderItem) =>
+    //       orderItem.id === item.id
+    //         ? { ...orderItem, quantity: orderItem.quantity + 1 }
+    //         : orderItem
+    //     );
+    //     setOrder(updatedOrder);
+    //     ("si existe");
+    //   } else {
+    //     const newItem = { ...item, quantity: 1 };
+    //     setOrder([...order, newItem]);
+    //     console.log("no existe");
+    //   }
   };
+
+  // console.log(order);
 
   return {
     addItem,
