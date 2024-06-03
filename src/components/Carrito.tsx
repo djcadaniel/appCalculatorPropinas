70398e47065323003595932b74d72f080b30c917
daniel castro <djcadaniel@gmail.com>
2024-06-03 17:15:54 -0500

    add btnCar

--- src/components/Carrito.tsx
+++ src/components/Carrito.tsx
@@ -12,7 +12,7 @@ const {y} = useScrollPosition();
 
   return (
     <button 
-      className={`z-50 w-[40px] fixed right-0 sm:right-10 3xl:right-72 ${ y > 800 ?'carritoScroll' : ''} transition-all duration-300 ease-in-out outline-none`}
+      className={`z-50 w-[40px] fixed right-0 top-10 sm:right-10 3xl:right-72 ${ y > 800 ?'carritoScroll' : ''} transition-all duration-300 ease-in-out outline-none`}
       onClick={showDetails}
     >
       <RiShoppingCartFill className="w-[30px] sm:w-[40px] h-auto text-amber-300 transition-text duration-300 ease-in-out hover:text-white" />
